9708714522a32cb995c1299c3f37602c7bfb1111
Pavani <vemasani.pavani@gmail.com>
2024-04-29 14:13:06 +0530

    added availability_zone

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ resource "aws_vpc" "vpc" {
 resource "aws_subnet" "subnet" {
   vpc_id     = aws_vpc.vpc.id
   cidr_block = "10.0.1.0/24"
-  availability_zone     = "us-east-1"
+  availability_zone     = "us-east-1a"
 
   tags = {
     Name = "zone1"
