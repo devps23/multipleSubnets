61bb365fc4aa0a6d92541a1aad61c9bf3e9803a8
Pavani <vemasani.pavani@gmail.com>
2024-04-29 14:37:30 +0530

    peer connection between both sided

--- main.tf
+++ main.tf
@@ -22,3 +22,8 @@ resource "aws_vpc_peering_connection" "peer" {
     Name = "peer-dev"
   }
 }
+resource "aws_route" "r" {
+  route_table_id            = "rtb-0a2e9ff93585c96fd"
+  destination_cidr_block    = "10.10.0.0/24"
+  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+}
