97d50a35a39b26f1a5b76ced395f1300b28b9d2c
Pavani <vemasani.pavani@gmail.com>
2024-04-29 14:07:58 +0530

    peer connection between two servers

--- main.tf
+++ main.tf
@@ -1,6 +1,24 @@
-resource "aws_vpc" "main" {
+resource "aws_vpc" "vpc" {
   cidr_block       = "10.10.0.0/24"
   tags = {
     Name = "vpc-aut-dev"
+  }
+}
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.1.0/24"
+  region     = "us-east-1"
+
+  tags = {
+    Name = "zone1"
+  }
+}
+resource "aws_vpc_peering_connection" "peer" {
+  peer_vpc_id   = "vpc-02a94ee8944923438"
+  vpc_id        = aws_vpc.vpc.id
+  auto_accept   = true
+
+  tags = {
+    Name = "peer-dev"
   }
 }
