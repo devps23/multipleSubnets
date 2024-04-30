3b1b1b9b0f2e2e6e578d85a70c5f0a0833cfc387
Pavani <vemasani.pavani@gmail.com>
2024-04-30 17:11:29 +0530

    create public subnets

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -50,7 +50,7 @@ resource "aws_subnet" "public_subnets" {
   availability_zone    = var.availability_zone[count.index]
 
   tags = {
-    Name = "${var.env}-public-subnet-${count.index}"
+    Name = "${var.env}-public-subnet-${count.index+1}"
   }
 }
 //peer connection between two vpc's
@@ -64,14 +64,14 @@ resource "aws_vpc_peering_connection" "peer" {
   }
 }
 //routing on both sides with customized peer connection
-resource "aws_route" "custom_vpc" {
-  route_table_id            =  var.default_route_table_id
-  destination_cidr_block    = var.cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
-resource "aws_route" "default_vpc" {
-  route_table_id            =  aws_vpc.vpc.main_route_table_id
-  destination_cidr_block    = var.default_cidr_block
-  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
-}
+//resource "aws_route" "custom_vpc" {
+//  route_table_id            =  var.default_route_table_id
+//  destination_cidr_block    = var.cidr_block
+//  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+//}
+//resource "aws_route" "default_vpc" {
+//  route_table_id            =  aws_vpc.vpc.main_route_table_id
+//  destination_cidr_block    = var.default_cidr_block
+//  vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
+//}
 
