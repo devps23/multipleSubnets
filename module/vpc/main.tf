234e5b21b1aa8f0a90e06ae07d71cb5a775345ae
Pavani <vemasani.pavani@gmail.com>
2024-04-30 17:14:35 +0530

    create public subnets

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -4,46 +4,7 @@ resource "aws_vpc" "vpc" {
     Name = "${var.vpc_name} - ${var.env}"
   }
 }
-//resource "aws_subnet" "subnet" {
-//  vpc_id     = aws_vpc.vpc.id
-//  cidr_block       = var.subnet_cidr_block
-//  availability_zone     = var.available_zone
-//
-//  tags = {
-//    Name = "zone"
-//  }
-//}
-resource "aws_subnet" "frontend" {
-  count                = length(var.frontend_subnet)
-  vpc_id               = aws_vpc.vpc.id
-  cidr_block           = var.frontend_subnet[count.index]
-  availability_zone    = var.availability_zone[count.index]
-
-  tags = {
-    Name = "${var.env}-frontend-subnet-${count.index}"
-  }
-}
-resource "aws_subnet" "backend" {
-  count                = length(var.backend_subnet)
-  vpc_id               = aws_vpc.vpc.id
-  cidr_block           = var.backend_subnet[count.index]
-  availability_zone    = var.availability_zone[count.index]
-
-  tags = {
-    Name = "${var.env}-backend-subnet-${count.index}"
-  }
-}
-resource "aws_subnet" "mysql" {
-  count                = length(var.mysql_subnet)
-  vpc_id               = aws_vpc.vpc.id
-  cidr_block           = var.mysql_subnet[count.index]
-  availability_zone    = var.availability_zone[count.index]
-
-  tags = {
-    Name = "${var.env}-mysql-subnet-${count.index}"
-  }
-}
-resource "aws_subnet" "public_subnets" {
+resource "aws_subnet" "public" {
   count                = length(var.public_subnets)
   vpc_id               = aws_vpc.vpc.id
   cidr_block           = var.public_subnets[count.index]
@@ -53,16 +14,56 @@ resource "aws_subnet" "public_subnets" {
     Name = "${var.env}-public-subnet-${count.index+1}"
   }
 }
-//peer connection between two vpc's
-resource "aws_vpc_peering_connection" "peer" {
-  peer_vpc_id   = var.default_vpc_id
-  vpc_id        = aws_vpc.vpc.id
-  auto_accept   = true
-
-  tags = {
-    Name = "peer-dev"
-  }
-}
+//resource "aws_subnet" "subnet" {
+//  vpc_id     = aws_vpc.vpc.id
+//  cidr_block       = var.subnet_cidr_block
+//  availability_zone     = var.available_zone
+//
+//  tags = {
+//    Name = "zone"
+//  }
+//}
+//resource "aws_subnet" "frontend" {
+//  count                = length(var.frontend_subnet)
+//  vpc_id               = aws_vpc.vpc.id
+//  cidr_block           = var.frontend_subnet[count.index]
+//  availability_zone    = var.availability_zone[count.index]
+//
+//  tags = {
+//    Name = "${var.env}-frontend-subnet-${count.index}"
+//  }
+//}
+//resource "aws_subnet" "backend" {
+//  count                = length(var.backend_subnet)
+//  vpc_id               = aws_vpc.vpc.id
+//  cidr_block           = var.backend_subnet[count.index]
+//  availability_zone    = var.availability_zone[count.index]
+//
+//  tags = {
+//    Name = "${var.env}-backend-subnet-${count.index}"
+//  }
+//}
+//resource "aws_subnet" "mysql" {
+//  count                = length(var.mysql_subnet)
+//  vpc_id               = aws_vpc.vpc.id
+//  cidr_block           = var.mysql_subnet[count.index]
+//  availability_zone    = var.availability_zone[count.index]
+//
+//  tags = {
+//    Name = "${var.env}-mysql-subnet-${count.index}"
+//  }
+//}
+//
+////peer connection between two vpc's
+//resource "aws_vpc_peering_connection" "peer" {
+//  peer_vpc_id   = var.default_vpc_id
+//  vpc_id        = aws_vpc.vpc.id
+//  auto_accept   = true
+//
+//  tags = {
+//    Name = "peer-dev"
+//  }
+//}
 //routing on both sides with customized peer connection
 //resource "aws_route" "custom_vpc" {
 //  route_table_id            =  var.default_route_table_id
