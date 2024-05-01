2b56ae7a2046fd2128c38026cf22bf791cddb3d3
Pavani <vemasani.pavani@gmail.com>
2024-05-01 13:55:30 +0530

    create single subnets,route

--- variables.tf
+++ variables.tf
@@ -1,13 +1,13 @@
-variable "cidr_block" {}
-variable "subnet_cidr_block"{}
-variable "vpc_name" {}
-variable "env" {}
-variable "available_zone"{}
-variable "default_vpc_id"{}
-variable "default_route_table_id"{}
-variable "default_cidr_block" {}
-variable "frontend-subnet"{}
-variable "backend-subnet"{}
-variable "mysql-subnet"{}
-variable "availability_zone" {}
-variable "public_subnets"{}
+//variable "cidr_block" {}
+//variable "subnet_cidr_block"{}
+//variable "vpc_name" {}
+//variable "env" {}
+//variable "available_zone"{}
+//variable "default_vpc_id"{}
+//variable "default_route_table_id"{}
+//variable "default_cidr_block" {}
+//variable "frontend-subnet"{}
+//variable "backend-subnet"{}
+//variable "mysql-subnet"{}
+//variable "availability_zone" {}
+//variable "public_subnets"{}
