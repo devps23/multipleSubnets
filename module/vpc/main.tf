f279eb6f1f4f7e011d5d34670aad169f45b29cf0
Pavani <vemasani.pavani@gmail.com>
2024-05-01 11:38:51 +0530

    attach internet gateway to public subnets

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -118,7 +118,7 @@ resource "aws_route_table" "public" {
     vpc_peering_connection_id = aws_vpc_peering_connection.peer.id
   }
   route {
-    cidr_block = 0.0.0.0/0
+    cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
   tags = {
