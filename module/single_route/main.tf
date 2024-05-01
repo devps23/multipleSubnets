7e975b598885cf48806b4ca0f908a9420c6b3adc
Pavani <vemasani.pavani@gmail.com>
2024-05-01 13:50:30 +0530

    create single subnets,route

---
+++ module/single_route/main.tf
@@ -0,0 +1,24 @@
+//create a new vpc
+resource "aws_vpc" "vpc" {
+  cidr_block       = "10.0.0.32/16"
+  tags = {
+    Name = "vpc-dev"
+  }
+}
+resource "aws_subnet" "subnet" {
+  vpc_id     = aws_vpc.vpc.id
+  cidr_block = "10.0.0.32/16"
+
+  tags = {
+    Name = "subnet-dev"
+  }
+}
+resource "aws_vpc_peering_connection" "peer" {
+  peer_vpc_id   = "pcx-00effd337b28a9d07"
+  vpc_id        = aws_vpc.vpc.id
+}
+//resource "aws_route" "r" {
+//  route_table_id            = aws_route_table.testing.id
+//  destination_cidr_block    = "10.0.1.0/22"
+//  vpc_peering_connection_id = "pcx-45ff3dc1"
+//}
