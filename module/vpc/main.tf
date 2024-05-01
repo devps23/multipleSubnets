c39a1a6757e5eeb52f41f22bf31f906e336c97cd
Pavani <vemasani.pavani@gmail.com>
2024-05-01 11:58:53 +0530

    create nat gateway

--- module/vpc/main.tf
+++ module/vpc/main.tf
@@ -121,6 +121,11 @@ resource "aws_route_table" "public" {
     cidr_block = "0.0.0.0/0"
     gateway_id = aws_internet_gateway.igw.id
   }
+  route {
+    cidr_block = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat.id
+  }
+
   tags = {
     Name = "public-rt-${var.env}-${count.index}"
   }
